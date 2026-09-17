22be72bd2d56468f1a17c844aed62cbfcde6ad79
Yuna <urimy01@gmail.com>
2026-09-17 16:29:12 -0700

    CY-4 Adding a Shift Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import ResetPassword from './pages/ResetPassword';
 import Home from './pages/Home';
-
+import ShiftForm from './pages/ShiftForm';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="/add-shift" element={<ShiftForm />} />
+            <Route path="/edit-shift/:slug" element={<ShiftForm />} />
+
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
   );
